13bb69ff23e9c8617a2939fdbbb0ef8af50b3fd5
Aymeric Pellé <aympelle@gmail.com>
2024-12-02 03:02:02 +0100

    v0.4.0: Add exit message in test_package.

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -7,5 +7,6 @@ int main()
 {
     vrsn::semver ver("0.1.0-dev+metadata");
     std::cout << std::format("version-{}", ver) << std::endl;
+    std::cout << "TEST PACKAGE SUCCESS" << std::endl;
     return EXIT_SUCCESS;
 }
